af971d47d4d2104d200695f29d1bccfc7713ba40
vivi-shiwei <55948716+vivi-shiwei@users.noreply.github.com>
2020-07-02 10:43:00 +0800

    Update Header.js

--- stepped-solutions/12/components/Header.js
+++ stepped-solutions/12/components/Header.js
@@ -8,7 +8,7 @@ const Header = props => (
         <span className="of">Of</span>
         <span className="the">The</span>
       </span>
-      Day
+      Dayf
     </h1>
     <h3 className="tagline">
       <span>{props.tagline}</span>
